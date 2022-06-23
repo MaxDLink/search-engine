01ca82dde1f499239004a7bf9839452ba016ec1a
Mode-man <max.durham.link@gmail.com>
2022-06-22 19:54:46 -0500

    parsed stop words from json files. Added stemmer library from @author Sean Massung.

--- main.cpp
+++ main.cpp
@@ -9,6 +9,11 @@
  *
  */
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <chrono>
+#include "porter2_stemmer.h"
+
 #include <filesystem>
 #include <fstream>
 #include <iomanip>
@@ -17,6 +22,7 @@
 //RapidJSON headers we need for our parsing.
 #include "rapidjson/istreamwrapper.h"
 #include "rapidjson/document.h"
+#include "stemmer.h"
 
 using namespace rapidjson;
 using std::cout;
@@ -58,6 +64,51 @@ int main(int argc,
     cout << "-------------------------------------------" << endl;
 //    testFileSystem("sample_data/");
     testFileSystem("own_file_data_sample/");
+
+
+//    ///stemmer
+//    string wordToStem = "running";
+//    Porter2Stemmer::stem(wordToStem);
+
+//    ///stemmer main //todo - clean up this block
+//    std::ifstream in{"data/diffs.txt"};
+//    std::string to_stem;
+//    std::string stemmed;
+//    bool mistake = false;
+//    using timer = std::chrono::high_resolution_clock;
+//    timer::time_point start_time = timer::now();
+//    while (in >> to_stem >> stemmed)
+//    {
+//        std::string orig = to_stem;
+//        Porter2Stemmer::trim(to_stem);
+//        Porter2Stemmer::stem(to_stem);
+//        if (to_stem != stemmed)
+//        {
+//            std::cout << "  incorrect!" << std::endl
+//                      << std::endl
+//                      << "to stem:  " << orig    << std::endl
+//                      << "stemmed:  " << to_stem << std::endl
+//                      << "expected: " << stemmed << std::endl;
+//            mistake = true;
+//        }
+//    }
+//    timer::time_point end_time = timer::now();
+//
+//    if (!mistake)
+//        std::cout << "Passed all tests!" << std::endl;
+//
+//    std::cout << "Time elapsed: "
+//              << std::chrono::duration_cast<std::chrono::milliseconds>(
+//                      end_time - start_time).count() << "ms" << std::endl;
+
+
+    ///stem test
+    string to_stem = "running";
+    cout << "unstemmed: " << to_stem << endl;
+    Porter2Stemmer::trim(to_stem);
+    cout << "trimmed: " << to_stem << endl;
+    Porter2Stemmer::stem(to_stem);
+    cout << "stemmed: " << to_stem << endl;
 
 
 }
@@ -140,7 +191,7 @@ void testReadJsonFile(const char *fileName) {
     }
 
     stopWordsFile.close();
-
+    //loop through and erase stopwords that are found in textContent vector
     for(int i = 0; i < stopWords.size(); i++){
         for(int j = 0; j < textContent.size(); j++){
             if(textContent.at(i) == stopWords.at(i)){
@@ -148,10 +199,12 @@ void testReadJsonFile(const char *fileName) {
             }
         }
     }
+    ///print text after stop word removal
+//    for(auto t: textContent){
+//        cout << "text after stopWord removal: " << t << endl;
+//    }
 
-    for(auto t: textContent){
-        cout << "text after stopWord removal: " << t << endl;
-    }
+
 }
 
 
